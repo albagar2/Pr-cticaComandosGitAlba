2bc8b987e9b8f1215e8df49f0f228b8b5427fb87
PC <PC@ordenador-alba2>
2023-10-19 20:56:59 +0200

    feat

--- src/Operaciones/Calculadora.java
+++ src/Operaciones/Calculadora.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Calculadora {
 	public static void main(String [] arg) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("1.Suma");
 		
+		System.out.println("1.Suma");
+		System.out.println("2.Resta");
 	}
 }
